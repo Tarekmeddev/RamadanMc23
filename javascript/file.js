7d66079c18731e1f9c63c94cdb498cf14a849d51
Tarekmeddev <116123972+Tarekmeddev@users.noreply.github.com>
2023-04-08 02:21:11 +0100

    Update file.js

--- javascript/file.js
+++ javascript/file.js
@@ -1,1 +1,21 @@
-let myArr = [35 , 50, 50, 15];
+// select elements
+let nav = document.querySelector(".ul");
+let navLi = document.querySelectorAll(".list");
+let btn = document.querySelector(".btn");
+let closeBtn = document.querySelector("div.x-mark");
+console.log(btn);
+/// open function
+btn.addEventListener("click", function () {
+  nav.style.cssText = "transform: translateX(0px);";
+  closeBtn.style.display = "block";
+});
+closeBtn.addEventListener("click", function () {
+  nav.style.cssText = "transform: translateX(-1000px);";
+  closeBtn.style.display = "none";
+});
+for (let i = 0; i < navLi.length; i++) {
+  navLi[i].addEventListener("click", function () {
+    nav.style.cssText = "transform: translateX(-1000px);";
+    closeBtn.style.display = "none";
+  });
+}
